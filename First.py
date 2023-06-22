e4056bbba2713cf15a9f8b525b96e4e4f553cdb1
t0shiik <toni.zot01@mail.ru>
2023-06-22 16:36:48 +0300

    Update file

--- First.py
+++ First.py
@@ -2,4 +2,26 @@
 # 1 вариант
 #1. Декоратор считающий количество вызовов функции
 
-print('Anton')
+def count_calls(func):
+    count = 0
+    def check(*args):
+        nonlocal count
+        count += 1
+        print(f'Функция {func.__name__} была вызвана {count} раз')
+        return func(*args)
+
+    return check
+
+# Пример использования
+@count_calls
+def Hello(name):
+    print(f'Hello, {name}')
+
+Hello('Anton')
+Hello('Hasbik')
+
+# Результат
+# Функция Hello была вызвана 1 раз
+# Hello, Anton
+# Функция Hello была вызвана 2 раз
+# Hello, Hasbik
